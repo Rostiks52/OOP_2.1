cf6ea3052ea403d5e51978201338ac90d03cf908
Rostiks52 <ksyu_05@mail.ru>
2021-05-27 05:30:26 +0300

    Files added

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -1,21 +1,15 @@
 package ru.netology.manager;
 
+import ru.netology.domain.Post;
+
 public class PostManager {
-    private WallSearch[] wallSearch;
-    private WallDelete[] wallDelete;
 
+    private Post[] posts;
 
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {}
 
 }
-//package ru.netology.manager;
-//
-//import ru.netology.domain.Movie;
-//
-//public class MovieManager {
-//  private Movie[] movies;
-//
-//  public Movie[] getMoviesForFeed() {
-//    // TODO: add logic
-//    return null;
-//  }
-//}
